97db208f376bdc4ba0b2081d70f8080cea05f621
Michelle Agcamaran <michelleaagcamaran@gmail.com>
2014-11-28 17:07:37 -0800

    current files for gui

---
+++ src/spaceteam/gui/HealthBar.java
@@ -0,0 +1,15 @@
+package spaceteam;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class HealthBar extends JPanel{
+	public HealthBar(){
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		
+	}
+}
---
+++ src/spaceteam/gui/JPanelWithBackground.java
@@ -0,0 +1,29 @@
+package spaceteam;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class JPanelWithBackground extends JPanel {
+
+	private Image backgroundImage;
+
+	public JPanelWithBackground(String fileName) {
+		try {
+			backgroundImage = ImageIO.read(new File(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+	    // Draw the background image.
+		g.drawImage(backgroundImage, 0, 0, this);
+	}
+}
