be71a7d478643086225b0fd3143d9aa2a9ab3b8f
Ananth Mohan <ananthmo@usc.edu>
2014-11-30 22:07:59 -0800

    added level finish logic

---
+++ src/spaceteam/server/messages/game/LevelFinish.java
@@ -0,0 +1,10 @@
+package spaceteam.server.messages.game;
+
+import spaceteam.server.messages.Message;
+
+/**
+ * Created by Ananth on 11/30/2014.
+ */
+public class LevelFinish implements Message
+{
+}
